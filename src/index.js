38052621ad43f95c63051acfe72c102a6afe10e9
songminseok <ske3056@gmail.com>
2023-08-13 21:10:16 +0900

    store and reducer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
+
+const countModifier = (count = 0) => {
+  return count;
+};
+
+//what is reducer? reducer is a function that modifies the data.
+const countStore = createStore(countModifier);
+
+console.log(countStore.getState());
 
 let count = 0;
 number.innerText = count;
