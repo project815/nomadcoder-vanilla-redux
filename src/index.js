d82d1a0a5ee49dfc3c25b7d510270fea04ee302a
songminseok <ske3056@gmail.com>
2023-08-13 21:30:23 +0900

    action

--- src/index.js
+++ src/index.js
@@ -4,31 +4,26 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  }
   return count;
 };
 
 //what is reducer? reducer is a function that modifies the data.
 const countStore = createStore(countModifier);
 
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 console.log(countStore.getState());
 
 let count = 0;
 number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
-};
-
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
-
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
