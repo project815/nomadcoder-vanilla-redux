b24ec1d31de946c300afc2375630269d89ebb0aa
songminseok <ske3056@gmail.com>
2023-08-15 23:53:33 +0900

    state mutation

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const reducer = (state = [], action) => {
   console.log(action.type, action.text);
   switch (action.type) {
     case TO_DO_ADD:
-      return [];
+      return [...state, { text: action.text, id: Date.now() }];
     case TO_DO_DELETE:
       return [];
     default:
@@ -21,6 +21,14 @@ const reducer = (state = [], action) => {
 };
 
 const store = createStore(reducer);
+
+//Redux Three Principles
+//1. Single source of truth
+//2. State is read-only
+//3. Changes are made with pure functions
+
+//store의 값(reducer의 state값)을 변경하기 위해서는 반드시 dispatch를 통해 action을 전달해야 한다.
+//state를 변경하는 것이 아니라 새로운 state를 만들어서 return한다.
 
 const createToDo = (toDo) => {
   const li = document.createElement("li");
@@ -35,6 +43,7 @@ const onSubmit = (e) => {
 
   store.dispatch({ type: TO_DO_ADD, text: toDo });
 
+  console.log(store.getState());
   createToDo(toDo);
 };
 
