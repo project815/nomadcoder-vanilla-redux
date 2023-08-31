f89d077fb6d23bb66f84f57559f7457eed90dc01
songminseok <ske3056@gmail.com>
2023-08-31 23:42:47 +0900

    setting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,30 +1,25 @@
 import React, { useState } from "react";
-import { connect } from "react-redux";
-import { actionCreators } from "../store";
-import ToDo from "../components/ToDo";
 
-function Home({ toDos, addToDo }) {
+function Home() {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
   }
   function onSubmit(e) {
     e.preventDefault();
-    addToDo(text);
+    console.log("text : ", text);
     setText("");
   }
   return (
     <>
-      <h1>To Do</h1>
+      <h1>To do</h1>
       <form onSubmit={onSubmit}>
-        <input type="text" value={text} onChange={onChange} />
-        <button>Add</button>
+        <input type="text" value={text} onChange={onChange}></input>
+        <button type="submit">Add</button>
       </form>
-      <ul>
-        {toDos.map((toDo) => (
-          <ToDo {...toDo} key={toDo.id} />
-        ))}
-      </ul>
+
+      <ul></ul>
     </>
   );
 }
+export default Home;
