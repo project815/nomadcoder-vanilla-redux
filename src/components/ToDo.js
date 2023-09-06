e2a7ad559ee0a5e8573778b7afd6710b3316240e
songminseok <ske3056@gmail.com>
2023-09-06 23:56:25 +0900

    tollkit, createAction

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,19 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
 import { actionCreators } from "../store";
-
-function ToDo({ text, onBtnClick }) {
+import { Link } from "react-router-dom";
+function ToDo({ text, onBtnClick, id }) {
   return (
     <li>
-      {text} <button onClick={onBtnClick}>DEL</button>
+      <Link to={`/${id}`}>{text}</Link>
+      <button onClick={onBtnClick}>DEL</button>
     </li>
   );
 }
-
-function mapDispatchToProps(dispatch, ownProps) {
-  return {
-    onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id)),
-  };
-}
-
-export default connect(null, mapDispatchToProps)(ToDo);
--- src/store.js
+++ src/store.js
@@ -1,25 +1,15 @@
 import { createStore } from "redux";
-const ADD = "ADD";
-const DELETE = "DELETE";
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id: parseInt(id),
-  };
-};
+import { createAction } from "@reduxjs/toolkit";
+
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
+    case addToDo.type:
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    case deleteToDo.type:
+      return state.filter((toDo) => toDo.id !== action.payload);
     default:
       return state;
   }
